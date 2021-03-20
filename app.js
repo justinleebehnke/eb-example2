23e6e8735ac627209ca9b84d35222a276f8ad88e
Justin Behnke <justin@databased.com>
2021-03-20 00:56:07 -0600

    Add cors

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var createError = require("http-errors");
+var cors = require("cors");
+
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
@@ -19,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(cors());
 app.use("/", mainServerRouter);
 
 /* START MY APPLICATION 
